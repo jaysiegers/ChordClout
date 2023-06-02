88b3bfa0358df875945f7441e2ee1a77e37d3d30
chellshelove <chellshelove733@gmail.com>
2023-06-02 23:04:51 +0700

    updated

--- .history/FrontEnd/src/App_20230602230323.jsx
+++ .history/FrontEnd/src/App_20230602230323.jsx
@@ -13,7 +13,7 @@ const App = () => {
       <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#a900b1]">
         {/* <Searchbar /> */}
 
-        <div className="px-6 h-[calc(100vh-0px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
+        <div className="px-6 h-[calc(100vh-50px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
           <div className="flex-1 h-fit pb-40">
             <Routes>
               <Route path="/" element={<Discover />} />
