eb0a4d454bedd60c9c8d727bf4ac366dcce9fb63
chellshelove <chellshelove733@gmail.com>
2023-06-05 03:28:56 +0700

    updated profile with tailwind

--- FrontEnd/src/assets/index.js
+++ FrontEnd/src/assets/index.js
@@ -1,9 +1,11 @@
 import loader from './loader.svg';
 import logo from './logo.png';
 import poster from './poster.png';
+import profile from './profile.png';
 
 export {
   logo,
   loader,
   poster,
+  profile,
 };
--- FrontEnd/src/components/Profile.jsx
+++ FrontEnd/src/components/Profile.jsx
@@ -1,16 +1,15 @@
 import { useState } from 'react';
 import {useNavigate} from 'react-router-dom';
-import { logo } from '../assets';
+import { profile } from '../assets';
 
 const Profile = () => {
 
   return (
     <button
-    className="p-2 text-lg mb-2 text-white bg-black rounded-lg"
+    className="absolute inset-y-0 right-0 p-2 text-lg mb-2 text-white bg-gradient-to-br from-black to-[#a900b1] w-20 h-20 py-[500px]"
     // onClick={() => logInWithEmailAndPassword(email, password)}
     >
-        <img src={logo.replace('{w}','10').replace('{h}','10')} alt="logo" className="rounded-full
-      object-cover border-2 shadow-xl shadow-black"/>
+        <img src={profile} alt="profile" className="rounded-full object-cover border-2 shadow-xl shadow-black"/>
     Profile
   </button>
 )
